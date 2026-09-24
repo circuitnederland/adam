f0238b775d4f6fb6b3a34dad1c64b14a20429f8d
Sandra Both <sandrab.git@gmail.com>
2026-09-24 11:10:47 +0200

    Avoid duplicate emandate record when redirect fails

--- src/main/emandates/CustomOperation_InternalCreate.groovy
+++ src/main/emandates/CustomOperation_InternalCreate.groovy
@@ -1,9 +1,22 @@
 import org.cyclos.entities.system.ExternalRedirectExecution
 import org.cyclos.entities.users.User
+import org.cyclos.model.NotificationException
 
 ExternalRedirectExecution execution = binding.execution
 Map<String, Object> formParameters = binding.formParameters
 User user = formParameters.user
 String bankId = formParameters.debtorBank.internalName
 
-return new EMandates(binding).newMandateRequest(user, execution, bankId)
+EMandates emandates = new EMandates(binding)
+
+// If there is a previous emandate without statusDate, the user did not return correctly.
+// Inform them to start over from the beginning - this will call updateStatus() still.
+def previousEM = emandates.newest(user)
+if (previousEM) {
+    def fields = scriptHelper.wrap(previousEM)
+    if (fields?.statusDate == null) {
+        NotificationException.raiseError(new Utils(binding).dynamicMessage("emMissingStatusMsg"))
+    }
+}
+
+return emandates.newMandateRequest(user, execution, bankId)
--- src/main/emandates/CustomOperation_InternalCreateCallback.groovy
+++ src/main/emandates/CustomOperation_InternalCreateCallback.groovy
@@ -2,8 +2,6 @@ import org.cyclos.entities.system.CustomFieldPossibleValue
 import org.cyclos.entities.system.ExternalRedirectExecution
 import org.cyclos.model.utils.RequestInfo
 
-import com.fasterxml.jackson.databind.ObjectMapper
-
 Map<String, String> scriptParameters = binding.scriptParameters
 ExternalRedirectExecution execution = binding.execution
 RequestInfo request = binding.request
@@ -11,7 +9,7 @@ def transactionId = request.getParameter('transactionId')
 
 def fields = new EMandates(binding).callback(execution, transactionId)
 
-// Inform the user about the result and return to the buy credits main operation screen (which only works in the app, on main the user returns to home).
+// Inform the user about the result and return to the main operation screen.
 String status = (fields.status as CustomFieldPossibleValue).internalName
 return [
     notification: new Utils(binding).dynamicMessage("emResult${status.capitalize()}"),
